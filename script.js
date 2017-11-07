0448e7a0a3120ad56c6a8c6449ecef600a33e15f
DeepBass <danbass8@googlemail.com>
2017-11-07 10:42:59 +0000

    update

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ var fullName = "Daniel Bass";
 var bob = new Product();
 console.log(fullName);
 var x;
+x = ["bob", 1];
 x[0] = "bob";
 x[1] = 2;
 //enums
@@ -29,3 +30,18 @@ var list = ['iPhone X', 'Surface Pro', 'Pixel 2', 'bob', 'dave'];
 var first = list[0], second = list[1], third = list[2], remaining = list.slice(3);
 console.log(first);
 console.log(remaining);
+//Object destructuring. Allows us to easily create variables to use on their own from properties of an object.
+var iphone = {
+    quality: "poor",
+};
+var quality = iphone.quality;
+console.log(quality);
+function setDefault(employee) {
+    //Question mark means nullable.
+    // The '=' sign sets a default value
+    var empName = employee.empName, _a = employee.companyName, companyName = _a === void 0 ? "Google" : _a;
+    console.log(empName);
+    console.log(companyName);
+}
+setDefault({ empName: 'Bass' });
+setDefault({ empName: 'Bass', companyName: 'bob' });
--- script.ts
+++ script.ts
@@ -13,6 +13,7 @@ let bob: Product = new Product();
 console.log(fullName);
 
 let x: [string,number];
+x = ["bob",1];
 x[0] = "bob";
 x[1] = 2;
 
@@ -26,3 +27,23 @@ let list = ['iPhone X', 'Surface Pro', 'Pixel 2','bob','dave'];
 let [first,second,third,...remaining] = list;
 console.log(first);
 console.log(remaining);
+
+
+//Object destructuring. Allows us to easily create variables to use on their own from properties of an object.
+let iphone = {
+    quality:"poor",
+};
+
+let { quality } = iphone;
+console.log(quality);
+
+function setDefault(employee: {empName: string, companyName?:string}){
+    //Question mark means nullable.
+
+    // The '=' sign sets a default value
+    let {empName, companyName = "Google"} = employee;
+    console.log(empName);
+    console.log(companyName);
+}
+setDefault({empName:'Bass'});
+setDefault({empName: 'Bass',companyName:'bob'});
